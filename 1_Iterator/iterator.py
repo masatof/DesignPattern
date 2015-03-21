1ffba3c7079ca4e65b032d5b4b9de18b4ef42c35
Masato Fukuyama <masato.fukuyama@gmail.com>
2015-03-21 23:26:43 +0900

    Change to use the abc

--- 1_Iterator/iterator.py
+++ 1_Iterator/iterator.py
@@ -1,8 +1,14 @@
-class Iterator(object):
+from abc import ABCMeta, abstractmethod
 
+
+class Iterator(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
     def has_next(self):
         pass
 
+    @abstractmethod
     def next(self):
         pass
 
@@ -25,7 +31,9 @@ class BookShelfIterator(Iterator):
 
 
 class Aggregate(object):
+    __metaclass__ = ABCMeta
 
+    @abstractmethod
     def iterator(self):
         pass
 
